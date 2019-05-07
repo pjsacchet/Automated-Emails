506eb6fbb9ece702646d7de52ec98469f7a31ec6
Patrick Sacchet <pjsacchet@loyola.edu>
2019-05-07 16:59:09 -0400

    Initial py file

---
+++ automated.py
@@ -0,0 +1,3 @@
+# Patrick Sacchet
+# Version 1.0 - 5/7/19
+# This program will take contacts fed to it via text file, create a SMTP server and send all contacts an email containing their training schedule
