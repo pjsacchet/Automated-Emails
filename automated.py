51f3895d08bc7e46215f3a03f902d156f701f2bf
Patrick Sacchet <pjsacchet@loyola.edu>
2019-05-12 20:18:11 -0400

    Additional changes for testing

--- automated.py
+++ automated.py
@@ -1,6 +1,7 @@
 # @author -  Patrick Sacchet
 # @version 1.0 - 5/7/19
-# This program will take contacts fed to it via text file, create a SMTP server and send all contacts an email containing their training schedule
+# This program will take contacts fed to it via text file, create a SMTP server and send all contacts an email containing their
+#    training schedule
 
 import smtplib
 import getpass
